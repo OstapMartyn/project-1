02e391e7707a94b59d86978823270e9c6adb6d1e
Ostap <Martinostap81@gmail.com>
2022-10-22 13:41:46 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+class Student:
+    def __init__(self, name, surname, age, group):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.group = group
+        self.marks = marks
+    def info(self):
+        print(f"Student {self.name} {self.surname}")
+
+    def average(self):
+        result = sum(self.marks) / len(self.marks)
+
+student1 = Student(
+    "Oleg",
+    "Bobko",
+    24,
+    "F18",
+    [9, 7, 2, 7, 12]
+)
+student2 = Student(
+    "Igor",
+    "Vovk",
+    28,
+    "F13",
+    [9, 12, 7, 12, 12, 5]
+)
+student1.info()
+student2.info()
+print(student1.average())
+print(student2.average())
