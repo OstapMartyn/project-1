991918e22aa0d542cadf3848682d6f71ba59a502
Ostap <Martinostap81@gmail.com>
2022-10-22 21:28:32 +0300

    Student work

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("Добрий день! Козовий Ігор Романович, бажаю вам гароного дня. ")
+class Student:
+    def init(self, name, surname, age, group, money):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.group = group
+        self.marks = marks
+        self.money = money
+    def info(self):
+        print(f"Student {self.name} {self.surname}")
+
+    def average(self):
+        result = sum(self.marks) / len(self.marks)
+
+    def info(self):
+        print(f"Student {self.money}")
+
+    def to_chill(self):
+        self.money -= 10
+
+    if self.money = 0
+      else:
+    print("I need go work to have money")
+
+    if self.marks = 5:
+       else:
+       print("I have to go to school to get good grades")
+
+student1 = Student(
+    "Oleg",
+    "Bobko",
+    24,
+    "F18",
+    [9, 7, 2, 7, 12]
+    500
+)
+student2 = Student(
+    "Igor",
+    "Vovk",
+    28,
+    "F13",
+    [9, 12, 7, 12, 12, 5]
+    541
+)
+student1.info()
+student2.info()
+print(student1.average())
+print(student2.average())
