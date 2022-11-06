8196acf1c7611c17c249d92143c8a6b9ffc40b05
Ostap <Martinostap81@gmail.com>
2022-11-06 12:10:18 +0200

    DZ_05.11.2022

--- main.py
+++ main.py
@@ -1,51 +1,7 @@
-class Student:
-    def init(self, name, surname, age, group, money):
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.group = group
-        self.marks = marks
-        self.money = money
-        self.gradebook = gradebook
-    def info(self):
-        print(f"Student {self.name} {self.surname}")
+user_input = input('Please provide an integer: ')
 
-    def average(self):
-        result = sum(self.marks) / len(self.marks)
-
-    def info(self):
-        print(f"Student {self.money}")
-
-    def to_chill(self):
-        self.money -= 10
-      if self.money = 0
-      else:
-    print("i need go work to have money")
-
-class Aspirant(Student):
-    student1:
-    print("Oleg Bobko is preparing to defend the thesis")
-    student2:
-    print("Igot Vovk isn`t preparing to defend the thesis")
-
-
-student1 = Student(
-    "Oleg",
-    "Bobko",
-    24,
-    "F18",
-    [9, 7, 2, 7, 12]
-    500
-)
-student2 = Student(
-    "Igor",
-    "Vovk",
-    28,
-    "F13",
-    [9, 12, 7, 12, 12, 5]
-    541
-)
-student1.info()
-student2.info()
-print(student1.average())
-print(student2.average())
+try:
+    int(user_input)
+    print(f'RESULT: {user_input}')
+except ValueError as error:
+    print(f'ERROR: {error}\nPlease provide an integer!')
