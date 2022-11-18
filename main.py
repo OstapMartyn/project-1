a206cda708ee56a239bc40236eb5f15142186912
Ostap <Martinostap81@gmail.com>
2022-11-18 20:53:06 +0200

    DZ_19.11.2022

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-import urllib.request
 
-opener = urllib.request.build_opener()
-response = opener.open("//bank.gov.ua/ua/markets/exchangerates")#
-print(response.read())
-import requests
-response = requests.get('//bank.gov.ua/ua/markets/exchangerates')#
-response_text = response.text
-response_span = response_text.split('<span>')
-for parse_elem_1 in response_span:
-    if parse_elem_1.startswith("$, ₽, €, zł"):
-        for parse_elem_2 in parse_elem_1.split('</span>'):
-            if parse_elem_2.startswith('$')("₽")("€")("zł") and parse_elem_2[36.00][0.01][36.5][7.5].isdigit():
-                print(parse_elem_2
-from bs4 import BeautifulSoup
-import requests
+name = Ala
 
-response = requests.get('//bank.gov.ua/ua/markets/exchangerates')
+CREATE TABLE Products(
+"id INT PRIMARY KEY NOT NULL,name CHAR(50) NOT NULL, price INT NOT NULL,quantity INT NOT NULL");
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, features='html.parser')
-    soup_list = soup.find_all(
-        'a',
-        {
-            'href': '//bank.gov.ua/ua/markets/exchangerates'
-        }
-    )
-    res = soup_list[0].find('span')
-    print(res.text)
+user_command = input('Please write an operation you want to do (“ADD PRODUCT” or “DELETE PRODUCT” or “SEE THE LIST OF PRODUCTS”: ')
+
+if user_command == “ADD PRODUCT”
+
+if user_command == “SEE THE LIST OF PRODUCTS”
+
+SELECT * FROM Products;
